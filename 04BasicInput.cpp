880df28f83226b7c351c9c6bc32f7d4dfdb16e98
Charlie Andaya <cga062805@gmail.com>
2024-10-03 11:18:13 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string nN;
+	string CYL;
+	string Sch;
+	
+	cout<<"Nickname:\t";
+	cin>>nN;
+	cout<<"Course-Year Level:\t";
+	cin>>CYL;
+	cout<<"School:\t";
+	cin>>Sch;
+	cout<<"\nWow Congrats "<<nN<<" ! "<<CYL<<" is a nice course. And you are studying in "<<Sch<<" which is one of the Center of Excellence in Teritory Education.";
+	return 0;
+}
