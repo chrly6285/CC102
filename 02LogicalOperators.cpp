ef0536e443917b4374c7232e75604c34573aa782
Charlie Andaya <cga062805@gmail.com>
2024-11-13 12:57:24 +0800

    Add files via upload

---
+++ 02LogicalOperators.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    
+    cout << "What is your age?: ";
+    cin >> age;
+    
+    if(age < 16){
+        cout << "Too young to drive" << endl;
+    }
+    else if(age == 16){
+        cout << "Better clear the road" << endl;
+    }
+    else {
+        cout << "you are getting pretty old" << endl;
+    }
+    
+    return 0;
+}
